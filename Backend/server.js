faac6f8b49a2174438029fed4bd7d213b7cc7b5c
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2024-01-23 09:03:01 +0530

    added and updated razor pay

--- Backend/server.js
+++ Backend/server.js
@@ -1,43 +1,15 @@
-//sk_test_51OX3fOSE90z45ySNzRpFkQ9mPeKTyhogVO0r2x5kxKdA0AGNvKhhEwMiFTMgaWEOBewK8FwBc2biJrcxJ3zrGEVr006njNbtWz
-//Dal_Khichadi :- price_1OXE34SE90z45ySNrOmzaUGy
-//Fish_Thali :- price_1OXE4OSE90z45ySNZA05rAwE
-//Chiken_Thali :- price_1OXE5OSE90z45ySNtwb9CSfQ
-
+//import the packages 
 const express = require("express");
 var cors = require("cors");
-const stripe = require("stripe")(
-  "sk_test_51OX3fOSE90z45ySNzRpFkQ9mPeKTyhogVO0r2x5kxKdA0AGNvKhhEwMiFTMgaWEOBewK8FwBc2biJrcxJ3zrGEVr006njNbtWz"
-);
-
+const dotenv = require('dotenv');
+const paymentRoutes = require("./routes/payments");
+//config env variables 
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+//create the app 
 const app = express();
 app.use(cors());
-app.use(express.static("public"));
 app.use(express.json());
+app.use("/api/payment/", paymentRoutes);
 
-app.post("/checkout", async (req, res) => {
-  /*
-     
-    */
-  const items = req.body.items;
-  let lineItem = [];
-  items.forEach((item) => {
-    lineItem.push({
-      price: item.id,
-      quantity: item.quantity,
-    });
-  });
-  const session = await stripe.checkout.sessions.create({
-    line_items: lineItem,
-    mode: "payment",
-    success_url: "http://localhost:3000/success",
-    cancel_url: "http://localhost:3000/cancel",
-  });
-
-  res.send(
-    JSON.stringify({
-      url: session.url,
-    })
-  );
-});
-
-app.listen(5000, () => console.log("App is listening on 5000"));
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
--- Frontend/src/components/Cart/Cart.jsx
+++ Frontend/src/components/Cart/Cart.jsx
@@ -2,28 +2,49 @@ import React from "react";
 import { useContext } from "react";
 import { cartContext } from "../Utilities/context";
 import Cartproduct from "./Cartproduct";
+import axios from 'axios';
+
 
 function Cart() {
   const cart = useContext(cartContext);
-  const checkout = async () => {
-    await fetch("http://localhost:5000/checkout", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        items: cart.items,
-      }),
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        if (response.url) {
-          window.location.assign(response.url);
-        }
-      });
+  const handlePayment = async () => {
+    try {
+      const orderUrl = "http://localhost:5000/api/payment/orders";
+      const { data } = await axios.post(orderUrl, { amount: cart.getTotalCost().toFixed(2) });
+      console.log(data);
+      initPayment(data.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  const initPayment = (data) => {
+		const options = {
+			key: "rzp_test_rEMvp9G69eq7Ym",
+			amount: data.amount,
+			currency: data.currency,
+			description: "Test Transaction",
+			order_id: data.id,
+			handler: async (response) => {
+				try {
+					const verifyUrl = "http://localhost:5000/api/payment/verify";
+					const { data } = await axios.post(verifyUrl, response);
+					console.log(data);
+				} catch (error) {
+					console.log(error);
+				}
+			},
+			theme: {
+				color: "#3399cc",
+			},
+		};
+		const rzp1 = new window.Razorpay(options);
+		rzp1.open();
+	};
+
+
+
+
 
   return (
     <div className="container main">
@@ -39,7 +60,7 @@ function Cart() {
         <div className="cart-container">
           <h2 class="card-title">Total Price</h2>
           <p class="card-description">₹{cart.getTotalCost().toFixed(2)}</p>
-          <a href="#" class="cart-button" onClick={checkout}>
+          <a href="#" class="cart-button" onClick={handlePayment}>
             Checkout
           </a>
         </div>
